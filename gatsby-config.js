5180146ca0081a0beebd8fc2058513cc92a979a0
henridev <henri.dbel@gmail.com>
2020-05-05 09:56:45 +0200

    my commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,17 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `these pandas eat a lot`,
+  },
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
---
+++ src/pages/about.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+
+export default ({ data }) => {
+  const { site } = data
+  return (
+    <Layout>
+      <h1>{site.siteMetadata.title}</h1>
+      <p>
+        We're the only site running on your computer dedicated to showing the
+        best photos and videos of pandas eating lots of food.
+      </p>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
